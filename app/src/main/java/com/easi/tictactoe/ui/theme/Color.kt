ff0e5c13ec2f6f9cb0db166986731fa6e867b4a4
ssisirix361 <ssisirix@gmail.com>
2025-08-02 10:25:22 +0200

    Add icon (O & X) color

--- app/src/main/java/com/easi/tictactoe/ui/theme/Color.kt
+++ app/src/main/java/com/easi/tictactoe/ui/theme/Color.kt
@@ -7,3 +7,6 @@ val Blue = Color(0xFF3A4A8F)
 val LightBlue = Color(0xFF007ACC)
 
 val BlueGradient = listOf(Color(0xFF2C3E50), Color(0xFF4A90E2), Color(0xFF007ACC))
+
+val red = Color(0xFFFF6B6B)
+val cyan = Color(0xFF4ECDC4)
